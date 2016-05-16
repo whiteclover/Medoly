da3d57fd7003c67a76aa279db3e4cc73b9c8bed7
whiteclover <lyanghwy@gmail.com>
2016-05-16 18:19:41 +0800

    Added doc

--- medoly/kanon/ctx.py
+++ medoly/kanon/ctx.py
@@ -1,9 +1,19 @@
-
+#!/usr/bin/env python
 class AppContext(object):
+    """The application context
+    """
 
     def __init__(self):
+        #: the requet handler hook entries
         self.hooks = []
+
+        #: the reuqest handler classes
         self.routes = []
+
+        #: the athem apllcation config settings
+        self.settings = {}
+
+        #: the error exception handler hook callbacks
         self.error_pages = {}
 
     def attach(self, point, callback, failsafe, priority, kwargs):
