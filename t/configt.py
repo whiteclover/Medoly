c806a62457f950535a928f321eec9ec59db0ffef
whiteclover <lyanghwy@gmail.com>
2016-05-28 11:08:15 +0800

    Added SelectConfig unittest

--- t/configt.py
+++ t/configt.py
@@ -15,7 +15,7 @@
 # under the License.
 
 
-from medoly.config import ConfigFactory
+from medoly.config import ConfigFactory, SelectConfig
 from util import conf_path
 
 
@@ -51,3 +51,40 @@ class ConfigTest(unittest.TestCase):
         conf = """f = 1.25 """
         config = ConfigFactory.parse(conf)
         self.assertEqual(config.get_float("f"), 1.25)
+
+
+class SelectConfigTest(unittest.TestCase):
+
+    def setUp(self):
+        self.config = SelectConfig()
+        self.config.set("server.host", "localhost")
+        self.config.set("server.port", 80)
+
+    def test_get(self):
+        self.assertEqual(self.config.get("server.port"), 80)
+        self.assertIsNone(self.config.get("server.not_found"))
+        # set default
+        self.assertEqual(self.config.get("debug", True), True)
+
+    def test_set(self):
+        server_confg = self.config.get("server")
+        self.assertEqual(server_confg['host'], 'localhost')
+        self.assertEqual(server_confg['port'], 80)
+        self.assertEqual(self.config.get("server.port"), 80)
+
+    def test_update(self):
+        self.config.set("server.host", "localhost")
+        self.config.set("server.port", 80)
+        config = {"server": {"host": "host2", "port": 90}, "debug": True}
+        self.config.update(config)
+        self.assertEqual(self.config.get("debug"), True)
+        self.assertEqual(self.config.get("server.host"), "host2")
+
+    def test_in(self):
+
+        self.assertTrue("server" in self.config)
+        self.assertFalse("server.not_found" in self.config)
+
+    def test_delete(self):
+        self.config.delete("server.port")
+        self.assertIsNone(self.config.get("server.port"))
